566332e17d90a10410598140753e54308ac8982b
Ezequiel Diaz <ezequiel.diaz>
2023-12-25 17:53:38 -0300

    icon next ok with darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useTheme } from 'next-themes';
 import AboutMe from "@/components/about_me";
 import Header from "@/components/header";
 import Projects from "@/components/projects";
@@ -9,23 +11,11 @@ import CardSocials from "@/components/card_socials";
 import CardEmail from "@/components/card_email";
 import Gmail from "@/components/email";
 
-const projects = [
-  {
-    title: "Guardian Bank",
-    description:
-      "It is a final homebanking project carried out for the ITBA university.",
-    imageSrc: "/images/guardian_bank.PNG",
-    url: 'https://itbank-beta.vercel.app/',
-    darkMode: true,
-    technologies: [
-      { name: "Next.js", icon: "icons/next.svg" },
-      { name: "React", icon: "icons/react.svg" },
-      { name: "Tailwind", icon: "icons/tailwind.svg" },
-      { name: "Django", icon: "icons/django.svg" },
-      { name: "Sql", icon: "icons/sql.svg" },
-    ],
-  },
-];
+
+const darkIcon = "icons_header/moon.svg";
+const lightIcon = "icons_header/sun.svg";
+
+
 
 const socialsData = [
   {
@@ -33,8 +23,8 @@ const socialsData = [
     icons: [
       {
         icon: "icons_social/linkedin.svg",
-        name: 'Linkedin',
-        url: 'https://www.linkedin.com/in/jeremias-arrieta/'
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/jeremias-arrieta/",
       },
     ],
   },
@@ -43,17 +33,69 @@ const socialsData = [
     icons: [
       {
         icon: "icons_social/github.svg",
-        name: 'Github',
-        url: 'https://github.com/jerearrieta'
+        name: "Github",
+        url: "https://github.com/jerearrieta",
       },
     ],
   },
 ];
 
 export default function Home() {
+
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const { setTheme } = useTheme();
+
+  const projects = [
+    {
+      title: "Guardian Bank",
+      description:
+        "It is a final homebanking project carried out for the ITBA university.",
+      imageSrc: "/images/guardian_bank.PNG",
+      url: "https://itbank-beta.vercel.app/",
+      technologies: [
+        { name: "Next.js", icon: `${darkMode ? "icons/next.svg" : "icons/next_black.svg" }`},
+        { name: "React", icon: "icons/react.svg" },
+        { name: "Tailwind", icon: "icons/tailwind.svg" },
+        { name: "Django", icon: "icons/django.svg" },
+        { name: "Sql", icon: "icons/sql.svg" },
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme === "dark") {
+      setDarkMode(true);
+      setTheme('dark');
+    }
+  }, [setTheme]);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
   return (
     <main className="mx-auto max-w-3xl mb-10">
-      <Header />
+      <div className="sticky top-0 z-10">
+        <header className="flex justify-end items-center py-3 px-5 z-20 bg-white dark:bg-gray-800 bg-opacity-80 dark:bg-opacity-80 backdrop-blur-lg">
+          <div className="hover:opacity-80 dark:hover:opacity-90 transition-all">
+            <Image
+              style={{ cursor: "pointer" }}
+              src={darkMode ? darkIcon : lightIcon}
+              width="30"
+              height="30"
+              alt="Icons"
+              onClick={() => setDarkMode(!darkMode)}
+            />
+          </div>
+        </header>
+      </div>
       <AboutMe />
       <hr className="border border-dotted border-gray-200 dark:border-gray-700 rounded-md mt-16" />
       <Projects />
